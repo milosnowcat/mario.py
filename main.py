557481babc8702c555e256d6deb42b7b9002c226
milosnowcat <ramiro.alvarez@rahcode.com>
2023-09-07 14:00:01 -0600

    show bg

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 650
-screen_height = 650
+screen_width = 960
+screen_height = 540
+
+bg_img = pygame.image.load('assets/img/background.png')
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Mario')
 
 run = True
 while run:
+    screen.blit(bg_img, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    pygame.display.update()
 
 pygame.quit()
