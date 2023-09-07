3606be5025b27ffac65b33660cd012b019a6d511
milosnowcat <ramiro.alvarez@rahcode.com>
2023-09-07 14:36:39 -0600

    make world

--- main.py
+++ main.py
@@ -16,17 +16,57 @@ pygame.display.set_caption('Mario')
 
 tile_size = 60
 
-bg_img = pygame.image.load('assets/img/background.png')
+background = pygame.image.load('assets/img/background.png')
+bg_img = pygame.transform.scale(background, (screen_width, screen_height))
 
 def draw_grid():
     for line in range(0,17):
         pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
         pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        dirt_img = pygame.image.load('assets/img/dirt.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world_data = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+]
+
+world = World(world_data)
+
 run = True
 while run:
     screen.blit(bg_img, (0, 0))
 
+    world.draw()
     draw_grid()
 
     for event in pygame.event.get():
