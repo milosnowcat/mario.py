b97b1da1810e50747e52503c305a124cfcf030aa
milosnowcat <ramiro.alvarez@rahcode.com>
2023-09-07 13:51:25 -0600

    TODO

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO hacer un juego en pygame inspirado en Mario,
+# tendra un nivel, conteo de puntos, mecanicas de salto,
+# movimiento horizontal y vertical.
+# Usará sprites gratuitos de itch.io
